b5f9f91390259c31c6a7ed1a0dd4e388489c05aa
cece <crodaemail@gmail.com>
2026-02-20 02:58:01 -0800

    initial commit

---
+++ AVANT_GARDE_PORTFOLIO_FILES/src/components/index.ts
@@ -0,0 +1,15 @@
+export { GlassHeader, Dock, InfoDrawer } from './Layout';
+export { CommandPalette } from './CommandPalette';
+export { LiquidBackground } from './LiquidBackground';
+export { AnalogClock } from './Clock';
+export {
+  ProfileView,
+  SplashView,
+  WebappsView,
+  WebtoolsView,
+  ArticlesView,
+  WebpagesView,
+  ProjectDetailView,
+  WebpageDetailView,
+  ArticleDetailView,
+} from './Views';
